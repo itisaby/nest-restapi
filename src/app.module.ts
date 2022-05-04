cf79aeea5a3d6b5e52ad46692c3c32efa4296fc7
itisaby <arnabbibhuti01@gmail.com>
2022-05-04 18:52:55 +0530

    mongo db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { authModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { productModule } from './products/products.module';
-
-
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config';
+const password = process.env.PWD;
 @Module({
-  imports: [authModule, UserModule, productModule],
+  imports: [authModule, UserModule, productModule, ConfigModule.forRoot(), MongooseModule.forRoot(
+    `mongodb+srv://node-rest:${password}@cluster1.fstlx.mongodb.net/rstdb?retryWrites=true&w=majority`
+    )],
   
 })
 export class AppModule {}
